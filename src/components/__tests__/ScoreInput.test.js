1dd24df596c45a8ff4cef8a9004221b434816aa8
Karolina Poslek <karolina@poslek.com>
2023-12-02 14:29:46 +0100

    add test for score input

--- src/components/__tests__/ScoreInput.test.js
+++ src/components/__tests__/ScoreInput.test.js
@@ -1,16 +1,16 @@
-import { describe, it, expect } from 'vitest';
-import { createPinia, setActivePinia } from 'pinia';
-import { mount } from '@vue/test-utils';
-import ScoreInput from './../ScoreInput.vue';
-import { useScoreStore } from '../../../stores/scoreStore';
+import { describe, it, expect } from 'vitest'
+import { createPinia, setActivePinia } from 'pinia'
+import { mount } from '@vue/test-utils'
+import ScoreInput from './../ScoreInput.vue'
+import { useScoreStore } from '../../../stores/scoreStore'
 
 describe('ScoreInput', () => {
   it('renders properly', () => {
-    setActivePinia(createPinia());
-    const store=useScoreStore()
-    store.score= "Hello from Store!"
-    const wrapper = mount(ScoreInput);
-    expect(wrapper.text()).toContain("Hello from Store!");
-    expect(wrapper.text()).toContain('Hello from ScoreInput!');
-  });
-});
+    setActivePinia(createPinia())
+    const store = useScoreStore()
+    store.score = 'Hello from Store!'
+    const wrapper = mount(ScoreInput)
+    expect(wrapper.text()).toContain('Hello from Store!')
+    expect(wrapper.text()).toContain('Hello from ScoreInput!')
+  })
+})
