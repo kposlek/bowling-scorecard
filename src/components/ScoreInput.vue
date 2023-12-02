250fa66e176ea50fdf346f23f695bbb16592c6d1
Karolina Poslek <karolina@poslek.com>
2023-12-02 14:27:38 +0100

    setting the input capability in the ScoreInput.vue component

--- src/components/ScoreInput.vue
+++ src/components/ScoreInput.vue
@@ -1,14 +1,58 @@
 <template>
-    <div>Hello from ScoreInput!</div>
-    {{ store.score }}
+  <div>
+    <div class="input-group mb-3 col">
+      <input
+        type="number"
+        class="form-control d-block"
+        placeholder="Insert number of frame"
+        aria-label="Insert number of frame"
+        aria-describedby="button-addon2"
+      />
+      <button class="btn btn-outline-secondary" type="button" id="button-addon2">
+        Add Number of Frame
+      </button>
+    </div>
+  </div>
+  <div class="row">
+    <div class="input-group mb-3 col">
+      <input
+        type="number"
+        class="form-control d-block"
+        placeholder="Insert number of the pins hit"
+        aria-label="Insert number of the pins hit"
+        aria-describedby="button-addon2"
+      />
+      <button class="btn btn-outline-secondary" type="button" id="button-addon2">Add pins</button>
+    </div>
+
+    <div class="input-group mb-3 col">
+      <input
+        type="number"
+        class="form-control d-block"
+        placeholder="Add roll number"
+        aria-label="Add roll number"
+        aria-describedby="button-addon2"
+      />
+      <button class="btn btn-outline-secondary" type="button" id="button-addon2">Add roll</button>
+    </div>
+    <div class="input-group mb-3 col">
+      <input
+        type="number"
+        class="form-control d-block"
+        placeholder="Insert number frame"
+        aria-label="Insert number frame"
+        aria-describedby="button-addon2"
+      />
+      <button class="btn btn-outline-secondary" type="button" id="button-addon2">Add Frame</button>
+    </div>
+  </div>
 </template>
 
 <script setup>
- import { useScoreStore } from '../../stores/scoreStore.js'
+import { useScoreStore } from '../../stores/scoreStore.js'
 
 const store = useScoreStore()
-console.log(store.score) 
+console.log(store.score)
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
