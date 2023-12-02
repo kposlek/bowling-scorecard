7ac4321e428d57c94cb6a9ae33f7e322aad8c681
Karolina Poslek <karolina@poslek.com>
2023-12-02 02:21:14 +0100

    creation of new components for input and display

---
+++ src/components/ScoreDisplay.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>Hello from ScoreDisplay!</div>
+    {{ store.score }}
+</template>
+
+<script setup>
+ import { useScoreStore } from '../../stores/scoreStore.js'
+
+const store = useScoreStore()
+console.log(store.score) 
+</script>
+
+<style scoped>
+</style>
