2ec339f1562d22603f344f65f1d0bb4c923f2480
Karolina Poslek <karolina@poslek.com>
2023-12-02 02:18:14 +0100

    restructuring App.vue and adding packages to the main.js file

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const app = createApp(App)
 
