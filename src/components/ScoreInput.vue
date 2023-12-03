cd971ab820ef66a5864c1301c517bc7da30b5550
Karolina Poslek <karolina@poslek.com>
2023-12-03 12:45:56 +0100

    setting validation on the input field

--- src/components/ScoreInput.vue
+++ src/components/ScoreInput.vue
@@ -1,25 +1,46 @@
 <template>
-  <div class="input-group mb-3">
+  <div class="input-group mb-3 w-50 d-flex">
     <input
-      type="number"
+      type="text"
       class="form-control d-block"
       :placeholder="store.static.inputPlaceholder"
       :aria-label="store.static.inputPlaceholder"
-      aria-describedby="button-addon2"
+      v-model="store.inputValue"
+      :class="{ 'is-invalid': store.errorMessage }"
     />
-    <PopoverInfo />
+    <!-- <PopoverInfo /> -->
     <button class="btn btn-outline-secondary" type="button" id="button-addon2">
       {{ store.static.addButton }}
     </button>
   </div>
+  <div v-if="store.errorMessage" class="d-block text-danger">
+    {{ store.errorMessage }}
+  </div>
 </template>
 
 <script setup>
-import PopoverInfo from './PopoverInfo.vue'
+import { watch } from 'vue'
 import { useScoreStore } from '../../stores/scoreStore.js'
 
 const store = useScoreStore()
-console.log(store.score)
+
+watch(
+  () => store.inputValue,
+  (newValue) => {
+    if (!newValue) {
+      store.errorMessage =
+        'This field is required. Please enter the number of bowling pins you knocked down.'
+    } else if (!store.inputValidation(newValue)) {
+      store.errorMessage = 'The entry is not valid. Please enter a value between 1 and 10.'
+    } else {
+      store.errorMessage = ''
+    }
+  }
+)
 </script>
 
-<style scoped></style>
+<style scoped>
+.is-invalid {
+  border-color: #dc3545;
+}
+</style>
--- stores/scoreStore.js
+++ stores/scoreStore.js
@@ -12,18 +12,26 @@ export const useScoreStore = defineStore('score', {
         popoverContent: 'Popover content'
       },
       score: 'Hello from Store!',
-
+      inputValue: '',
       frames: [1, 2, 3, 4, 5, 6, 7, 8, 9],
       frameResume: 0,
       hits: ['10', '10'],
 
       player: 'Karolina',
-      resume: 0
+      resume: 0,
+
+      //input validation
+      errorMessage: ''
     }
   },
   actions: {
     fetchScore() {
       console.log(this.score)
+    },
+    //validation od inputs value
+    inputValidation(value) {
+      const broj = parseInt(value)
+      return broj >= 1 && broj <= 10 && !isNaN(broj)
     }
   }
 })
