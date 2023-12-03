2332893759ea1bfc6d4112b7e489a7c5695c7c1d
Karolina Poslek <karolina@poslek.com>
2023-12-03 02:30:10 +0100

    placement of static elements in the store

--- stores/scoreStore.js
+++ stores/scoreStore.js
@@ -1,10 +1,24 @@
-
 import { defineStore } from 'pinia'
 
-export const useScoreStore = defineStore('score',  {
+export const useScoreStore = defineStore('score', {
   state: () => {
     return {
-      score: "Hello from Store!"
+      static: {
+        frameHeader: 'Frames',
+        playerHeader: 'Player',
+        totalHeader: 'Total',
+        addButton: 'Add pins',
+        inputPlaceholder: 'Number of the pins hit',
+        popoverContent: 'Popover content'
+      },
+      score: 'Hello from Store!',
+
+      frames: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      frameResume: 0,
+      hits: ['10', '10'],
+
+      player: 'Karolina',
+      resume: 0
     }
   },
   actions: {
