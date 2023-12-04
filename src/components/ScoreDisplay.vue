a7fbcfd894962343a5b3e4b830a7169845957977
Karolina Poslek <karolina@poslek.com>
2023-12-04 23:32:11 +0100

    setting up validation for display based on the import of input into a list

--- src/components/ScoreDisplay.vue
+++ src/components/ScoreDisplay.vue
@@ -19,20 +19,28 @@
 
           <div class="col p-0 border-bottom" v-for="frame in store.frames" :key="frame.id">
             <div class="row p-0 border-bottom m-0 justify-content-center">
-              <div class="col p-0" v-for="hit in store.hits" :key="hit.id">
-                <div class="hits-item border-end p-1">{{ hit }}</div>
+              <div class="col border-end p-1">
+                {{ printThrow1(frame.throw1) }}
+              </div>
+
+              <div class="col border-end p-1">{{ printThrow2(frame.throw2, frame.throw1) }}</div>
+
+              <div v-if="frame.id === 9" class="col border-end p-1">
+                {{ printThrow3(frame.throw3, frame.throw1) }}
               </div>
             </div>
-            <div class="row p-1 m-0 justify-content-center border-end">{{ store.frameScore }}</div>
+            <div class="row p-1 m-0 justify-content-center border-end">
+              {{ frame.score }}
+            </div>
           </div>
-          <div class="col p-0 border-bottom">
+          <!--  <div class="col p-0 border-bottom">
             <div class="row d-flex fw-bold m-0 p-1 justify-content-center border-bottom border-end">
               {{ store.static.totalHeader }}
             </div>
             <div class="row d-flex m-0 p-1 justify-content-center border-end">
-              {{ store.totalFrameScore }}
+              {{ store.totalScore }}
             </div>
-          </div>
+          </div> -->
         </div>
       </div>
     </div>
@@ -43,6 +51,53 @@
 import { useScoreStore } from '../../stores/scoreStore.js'
 import ScoreInput from './ScoreInput.vue'
 const store = useScoreStore()
+
+function printThrow1(frameThrow) {
+  if (frameThrow === undefined || frameThrow === 'strike') {
+    return '\u00A0'
+  } else if (frameThrow === 0) {
+    return '-'
+  } else if (frameThrow === 10) {
+    return 'X'
+  } else {
+    return frameThrow
+  }
+}
+
+function printThrow2(frameThrow, previousThrow) {
+  if (frameThrow === undefined) {
+    return '\u00A0'
+  } else {
+    if (previousThrow === 'strike') {
+      return 'X'
+    } else if (frameThrow + previousThrow === 10) {
+      return '/'
+    } else if (frameThrow === 0) {
+      return '-'
+    } else if (frameThrow === 10) {
+      return 'X'
+    } else {
+      return frameThrow
+    }
+  }
+}
+function printThrow3(frameThrow3, frameThrow1) {
+  if (frameThrow3 === undefined) {
+    return '\u00A0'
+  } else {
+    if (frameThrow1 === 10) {
+      if (frameThrow3 > 0 && frameThrow3 < 10) {
+        return frameThrow3
+      } else if (frameThrow3 === 0) {
+        return '-'
+      } else if (frameThrow3 === 10) {
+        return 'X'
+      }
+    } else {
+      return '\u00A0'
+    }
+  }
+}
 </script>
 
 <style scoped>
