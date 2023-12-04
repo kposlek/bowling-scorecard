80b8c2a0a020bc5f4210393b6fa4c94a375547ca
Karolina Poslek <karolina@poslek.com>
2023-12-04 23:50:46 +0100

    setting up an error message for invalid field input in the case of the second throw, where the sum of the first and second throws must not exceed 10, and remove unnecesary PopoverInfo component

--- src/components/ScoreInput.vue
+++ src/components/ScoreInput.vue
@@ -7,22 +7,34 @@
         class="form-control d-block"
         v-model="store.inputValue"
         :class="{ 'is-invalid': store.errorMessage }"
+        @keypress.enter="store.addInput()"
       />
       <!-- <PopoverInfo /> -->
       <button
         class="btn btn-outline-secondary d-md-block d-none rounded-end"
         type="button"
         id="button-addon2"
+        :class="{ 'is-invalid': store.errorMessage }"
+        @click="store.addInput()"
       >
         {{ store.static.addButton }}
       </button>
-      <button class="btn btn-outline-secondary d-md-none d-block" type="button" id="button-addon2">
+      <button
+        class="btn btn-outline-secondary d-md-none d-block"
+        type="button"
+        id="button-addon2"
+        :class="{ 'is-invalid': store.errorMessage }"
+        @click="store.addInput()"
+      >
         +
       </button>
     </div>
   </div>
-  <div v-if="store.errorMessage" class="d-block text-danger text-center">
+  <div v-if="store.errorMessage && !store.validationError" class="d-block text-danger text-center">
     {{ store.errorMessage }}
+  </div>
+  <div class="d-block text-danger fw-bold text-center">
+    {{ store.validationError }}
   </div>
 </template>
 
@@ -35,9 +47,7 @@ const store = useScoreStore()
 watch(
   () => store.inputValue,
   (newValue) => {
-    if (!newValue) {
-      store.inputRequiredError()
-    } else if (!store.inputValidation(newValue)) {
+    if (!store.inputValidation(newValue) && newValue != 0) {
       store.inputValidationError()
     } else {
       store.errorMessage = ''
--- stores/scoreStore.js
+++ stores/scoreStore.js
@@ -12,11 +12,9 @@ export const useScoreStore = defineStore('score', {
         //Headers for ScoreInput component
         addButton: 'Add pins',
         inputHeading: 'Number of pins hit:',
-        /* popoverContent: 'Popover content', */
 
         //Input validation messages
-        requiredInputMessage:
-          'This field is required. Please enter the number of bowling pins you knocked down.',
+        requiredInputMessage: 'The entry is not valid. The total sum of all pins can be up to 10.',
         validatedInputMessage: 'The entry is not valid. Please enter a value between 1 and 10.'
       },
       //test
@@ -42,7 +40,8 @@ export const useScoreStore = defineStore('score', {
 
       //input validation error message
       errorMessage: '',
-      validationError: ''
+      validationError: '',
+      valueAdded: false
     }
   },
   actions: {
@@ -51,9 +50,7 @@ export const useScoreStore = defineStore('score', {
       const pinsHit = parseInt(value)
       return pinsHit >= 1 && pinsHit <= 10 && !isNaN(pinsHit)
     },
-    inputRequiredError() {
-      return (this.errorMessage = this.static.requiredInputMessage)
-    },
+
     inputValidationError() {
       return (this.errorMessage = this.static.validatedInputMessage)
     },
@@ -61,42 +58,41 @@ export const useScoreStore = defineStore('score', {
     addInput() {
       const value = parseInt(this.inputValue)
       if (isNaN(value)) return
-      let valueAdded = false
+
       this.validationError = ''
 
       if (value >= 0 && value < 11) {
         this.frames.forEach((frame) => {
-          if (!valueAdded) {
+          if (!this.valueAdded) {
             if (frame.throw1 === undefined) {
               if (value === 10 && frame.id !== 9) {
                 frame.throw1 = 'strike'
                 frame.throw2 = 0
-                valueAdded = true
+                this.valueAdded = true
               } else {
                 frame.throw1 = value
-                valueAdded = true
+                this.valueAdded = true
               }
             } else if (frame.throw2 === undefined) {
               if (frame.id !== 9) {
                 if (frame.throw1 + value <= 10) {
                   frame.throw2 = value
-                  valueAdded = true
+                  this.valueAdded = true
                 } else {
-                  valueAdded = true
+                  this.valueAdded = true
                   this.errorMessage = ''
-                  return (this.validationError =
-                    'The entry is not valid. The sum of all pins can be up to 10.')
+                  return (this.validationError = this.static.requiredInputMessage)
                 }
               } else if (value >= 0 && value <= 10) {
                 frame.throw2 = value
-                valueAdded = true
+                this.valueAdded = true
               }
             } else if (frame.throw3 === undefined && frame.id === 9) {
               frame.throw3 = value
-              valueAdded = true
+              this.valueAdded = true
             }
           }
-          if (valueAdded) {
+          if (this.valueAdded) {
             this.inputValue = ''
           }
         })
