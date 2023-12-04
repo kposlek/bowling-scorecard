6ae1577cca987b4f4793843ccb3fd7a3e1e17d79
Karolina Poslek <karolina@poslek.com>
2023-12-04 23:23:40 +0100

    setting the initial state in the object list for each frame, importing and validating the input values in the list
     Committer: Karolina Poslek <karolina@poslek.com>

--- stores/scoreStore.js
+++ stores/scoreStore.js
@@ -11,7 +11,7 @@ export const useScoreStore = defineStore('score', {
 
         //Headers for ScoreInput component
         addButton: 'Add pins',
-        inputHeading: 'Number of the pins hit:',
+        inputHeading: 'Number of pins hit:',
         /* popoverContent: 'Popover content', */
 
         //Input validation messages
@@ -21,32 +21,88 @@ export const useScoreStore = defineStore('score', {
       },
       //test
       inputValue: '',
-      frames: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      frameScore: 0,
-
-      hits: ['10', '10'],
+      frames: [
+        { id: 0, throw1: undefined, throw2: undefined, score: 0 },
+        { id: 1, throw1: undefined, throw2: undefined, score: 0 },
+        { id: 2, throw1: undefined, throw2: undefined, score: 0 },
+        { id: 3, throw1: undefined, throw2: undefined, score: 0 },
+        { id: 4, throw1: undefined, throw2: undefined, score: 0 },
+        { id: 5, throw1: undefined, throw2: undefined, score: 0 },
+        { id: 6, throw1: undefined, throw2: undefined, score: 0 },
+        { id: 7, throw1: undefined, throw2: undefined, score: 0 },
+        { id: 8, throw1: undefined, throw2: undefined, score: 0 },
+        { id: 9, throw1: undefined, throw2: undefined, throw3: undefined, score: 0 }
+      ],
 
       //fictitious player
       player: 'Karolina',
 
       //
-      totalFrameScore: 0,
+      totalScore: 0,
 
       //input validation error message
-      errorMessage: ''
+      errorMessage: '',
+      validationError: ''
     }
   },
   actions: {
     //validation on inputs value
     inputValidation(value) {
-      const broj = parseInt(value)
-      return broj >= 1 && broj <= 10 && !isNaN(broj)
+      const pinsHit = parseInt(value)
+      return pinsHit >= 1 && pinsHit <= 10 && !isNaN(pinsHit)
     },
     inputRequiredError() {
       return (this.errorMessage = this.static.requiredInputMessage)
     },
     inputValidationError() {
       return (this.errorMessage = this.static.validatedInputMessage)
+    },
+    //Score input
+    addInput() {
+      const value = parseInt(this.inputValue)
+      if (isNaN(value)) return
+      let valueAdded = false
+      this.validationError = ''
+
+      if (value >= 0 && value < 11) {
+        this.frames.forEach((frame) => {
+          if (!valueAdded) {
+            if (frame.throw1 === undefined) {
+              if (value === 10 && frame.id !== 9) {
+                frame.throw1 = 'strike'
+                frame.throw2 = 0
+                valueAdded = true
+              } else {
+                frame.throw1 = value
+                valueAdded = true
+              }
+            } else if (frame.throw2 === undefined) {
+              if (frame.id !== 9) {
+                if (frame.throw1 + value <= 10) {
+                  frame.throw2 = value
+                  valueAdded = true
+                } else {
+                  valueAdded = true
+                  this.errorMessage = ''
+                  return (this.validationError =
+                    'The entry is not valid. The sum of all pins can be up to 10.')
+                }
+              } else if (value >= 0 && value <= 10) {
+                frame.throw2 = value
+                valueAdded = true
+              }
+            } else if (frame.throw3 === undefined && frame.id === 9) {
+              frame.throw3 = value
+              valueAdded = true
+            }
+          }
+          if (valueAdded) {
+            this.inputValue = ''
+          }
+        })
+      } else {
+        return
+      }
     }
   }
 })
