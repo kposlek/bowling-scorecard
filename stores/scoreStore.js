1658b6c08336b650bd674c000888d854e8c6270e
Karolina Poslek <karolina@poslek.com>
2023-12-05 02:44:44 +0100

    adding the first and second throw

--- stores/scoreStore.js
+++ stores/scoreStore.js
@@ -20,8 +20,8 @@ export const useScoreStore = defineStore('score', {
       //test
       inputValue: '',
       frames: [
-        { id: 0, throw1: undefined, throw2: undefined, score: 0 },
-        { id: 1, throw1: undefined, throw2: undefined, score: 0 },
+        { id: 0, throw1: 'strike', throw2: 0, score: 10 },
+        { id: 1, throw1: 2, throw2: 5, score: 0 },
         { id: 2, throw1: undefined, throw2: undefined, score: 0 },
         { id: 3, throw1: undefined, throw2: undefined, score: 0 },
         { id: 4, throw1: undefined, throw2: undefined, score: 0 },
@@ -40,8 +40,7 @@ export const useScoreStore = defineStore('score', {
 
       //input validation error message
       errorMessage: '',
-      validationError: '',
-      valueAdded: false
+      validationError: ''
     }
   },
   actions: {
@@ -58,47 +57,72 @@ export const useScoreStore = defineStore('score', {
     addInput() {
       const value = parseInt(this.inputValue)
       if (isNaN(value)) return
-
+      let valueAdded = false
       this.validationError = ''
 
       if (value >= 0 && value < 11) {
         this.frames.forEach((frame) => {
-          if (!this.valueAdded) {
+          if (!valueAdded) {
             if (frame.throw1 === undefined) {
               if (value === 10 && frame.id !== 9) {
                 frame.throw1 = 'strike'
                 frame.throw2 = 0
-                this.valueAdded = true
+                valueAdded = true
               } else {
                 frame.throw1 = value
-                this.valueAdded = true
+                valueAdded = true
               }
             } else if (frame.throw2 === undefined) {
               if (frame.id !== 9) {
                 if (frame.throw1 + value <= 10) {
                   frame.throw2 = value
-                  this.valueAdded = true
+                  valueAdded = true
                 } else {
-                  this.valueAdded = true
+                  valueAdded = true
                   this.errorMessage = ''
                   return (this.validationError = this.static.requiredInputMessage)
                 }
               } else if (value >= 0 && value <= 10) {
                 frame.throw2 = value
-                this.valueAdded = true
+                valueAdded = true
               }
             } else if (frame.throw3 === undefined && frame.id === 9) {
               frame.throw3 = value
-              this.valueAdded = true
+              valueAdded = true
             }
           }
-          if (this.valueAdded) {
+          if (valueAdded) {
             this.inputValue = ''
           }
         })
       } else {
         return
       }
+    },
+
+    returnNumber(value) {
+      if (value === undefined) {
+        return 0
+      } else if (value === 'strike') {
+        return 10
+      } else {
+        return value
+      }
+    },
+
+    //Total Score
+    totalScoreSum(value, index) {
+      console.log(this.totalScore)
+      if (index !== 9) {
+        let throw1 = this.returnNumber(this.frames[index + 1].throw1)
+        let throw2 = this.returnNumber(this.frames[index + 1].throw2)
+        //this.totalScore = this.totalScore + this.frames[index].score + throw1 + throw2
+        //this.totalScore = this.totalScore + this.frames[index].score + throw1 + throw2
+      } else {
+        console.log(222222222222222)
+      }
+
+      return this.totalScore
     }
   }
 })
